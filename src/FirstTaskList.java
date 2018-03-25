e9cfbad2f7e0ee128a5dc5801c0c23a48e3d370f
Ieva Sinkeviciute <ieva1919@gmail.com>
2018-03-25 20:24:29 +0300

    sarasas

--- src/FirstTaskList.java
+++ src/FirstTaskList.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 public class FirstTaskList {
     public static void main(String[] args) {
         /*Sukurti naują ArrayList kolekciją, pridėti 5 vartotojų vardus ir juos atspausdinti,
-        jei vardo ilgis ilgesnis nei 6 simboliai. Rezultatus spausdinti ekrane naudojant for ciklą.*/
+        jei vardo ilgis ilgesnis nei 6 simboliai. Rezultatus spausdinti ekrane naudojant for ciklą.
+        Naudojant sukurtą kolekciją parašyti du metodus. Pirmas grąžina pirmą list`o elementą, o
+        antras metodas paskutinį. Rezultatus spausdinti ekrane.*/
 
         ArrayList<String> names = new ArrayList<>();
 
@@ -13,15 +15,18 @@ public class FirstTaskList {
         names.add("Vaiva");
 
 
-        System.out.println("Vardai: " + names);
-
-        for(int i = 0; i< 6; i++) {
-
+        for (String name : names) {
+            if(name.length() > 6) {
+                System.out.println(name);
+            }
         }
-
-
-
-
-
+        System.out.println("Pirmas kolekcijos elementas: " + pirmasElementas(names));
+        System.out.println("Paskutinis kolekcijos elementas: " + paskutinisElementas(names));
+    }
+    private static String pirmasElementas (ArrayList<String>  names) {
+        return names.get(0);
+    }
+    private static String paskutinisElementas (ArrayList<String>  names) {
+        return names.get(names.size()-1);
     }
 }
